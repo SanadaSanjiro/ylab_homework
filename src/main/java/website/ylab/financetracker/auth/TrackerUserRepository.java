4337fe9324e4bdc6bc86cf1cdb220b4d0affcc8f
SanadaSanjiro <trollegon@narod.ru>
2025-03-11 23:26:30 +0800

    Work on mistakes

--- src/main/java/website/ylab/financetracker/auth/TrackerUserRepository.java
+++ src/main/java/website/ylab/financetracker/auth/TrackerUserRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TrackerUserRepository {
-    public Optional<TrackerUser> create(TrackerUser user);
-    public Optional<TrackerUser> get(String username);
-    public Optional<TrackerUser> update(TrackerUser oldUser, TrackerUser newUser);
-    public Optional<TrackerUser> delete(TrackerUser user);
-    public List<TrackerUser> getAllUsers();
+    Optional<TrackerUser> create(TrackerUser user);
+    Optional<TrackerUser> get(String username);
+    Optional<TrackerUser> update(TrackerUser oldUser, TrackerUser newUser);
+    Optional<TrackerUser> delete(TrackerUser user);
+    List<TrackerUser> getAllUsers();
 }
--- src/main/java/website/ylab/financetracker/transactions/TrackerTransactionRepository.java
+++ src/main/java/website/ylab/financetracker/transactions/TrackerTransactionRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 public interface TrackerTransactionRepository {
-    public Optional<TrackerTransaction> create(TrackerTransaction transaction);
-    public Optional<TrackerTransaction> update(TrackerTransaction transaction);
-    public Optional<TrackerTransaction> delete(TrackerTransaction transaction);
-    public Optional<TrackerTransaction> get(long id);
-    public List<TrackerTransaction> getAllTransactions();
+    Optional<TrackerTransaction> create(TrackerTransaction transaction);
+    Optional<TrackerTransaction> update(TrackerTransaction transaction);
+    Optional<TrackerTransaction> delete(TrackerTransaction transaction);
+    Optional<TrackerTransaction> get(long id);
+    List<TrackerTransaction> getAllTransactions();
 }
